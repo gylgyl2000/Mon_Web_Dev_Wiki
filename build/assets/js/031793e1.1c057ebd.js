94395c3128d44a4e28efe91a427147674602794e
gylgyl2000 <gylgyl2000@gmail.com>
2021-09-12 20:51:54 +0200

    build

--- build/assets/js/031793e1.1c057ebd.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmon_web_dev_wiki=self.webpackChunkmon_web_dev_wiki||[]).push([[633],{2511:function(e){e.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"/blog/tags/facebook","name":"facebook","count":1,"permalink":"/blog/tags/facebook"}')}}]);
--- build/assets/js/e16015ca.394c2f2f.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmon_web_dev_wiki=self.webpackChunkmon_web_dev_wiki||[]).push([[700],{5688:function(a){a.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"/blog/tags/hola","name":"hola","count":1,"permalink":"/blog/tags/hola"}')}}]);
